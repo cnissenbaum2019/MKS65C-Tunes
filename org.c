c3f43577c63e2783a3c983304fa3b808fe25aa0b
KevinLyy <33733915+KevinLyy@users.noreply.github.com>
2018-10-13 13:15:11 -0400

    Org

--- org.c
+++ org.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+struct song_node * table[27];
+
+//Add song nodes.
+struct song_node * add_song(){};
+
+//Search for a song given song and artist name (return a pointer).
+
+//Search for an artist.
+
+//Print out all the entries
+//under a certain letter.
+
+//Print out all the songs of a 
+//certain artist
+
+//Print out the entire library.
+
+//Shuffle - print out a series of 
+//randomly chosen songs.
+
+//Delete a song
+
+//Clear the library.
