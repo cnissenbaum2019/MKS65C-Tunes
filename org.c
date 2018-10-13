9b5ac6189338d6f2ed83b657b9a35d08d01be9aa
KevinLyy <33733915+KevinLyy@users.noreply.github.com>
2018-10-13 13:27:26 -0400

    Functions

--- org.c
+++ org.c
@@ -5,23 +5,31 @@
 struct song_node * table[27];
 
 //Add song nodes.
-struct song_node * add_song(){};
+struct song_node * add_song(song_node * name){};
 
-//Search for a song given song and artist name (return a pointer).
+//Search for a song given song and 
+//artist name (return a pointer).
+struct song_node * search_song(char song[], char artist[]){};
 
 //Search for an artist.
 
+
 //Print out all the entries
 //under a certain letter.
+struct song_node * printletter(char letter[]){};
 
 //Print out all the songs of a 
 //certain artist
+struct song_node * printartistlibrary(char name[]){};
 
 //Print out the entire library.
+void printlibrary(table){};
 
 //Shuffle - print out a series of 
 //randomly chosen songs.
+void shuffle(){};
 
 //Delete a song
+void deletesong(char song[]){};
 
 //Clear the library.
